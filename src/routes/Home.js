ffc9f26cc23bf34fee3e96d6777b922a69ea4b14
dinomoon <ansrud45@gmail.com>
2021-02-09 18:27:28 +0900

    Form add to Home and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    const { value } = e.target;
+    setNweet(value);
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="text"
+        value={nweet}
+        onChange={onChange}
+        type="text"
+        placeholder="What`s on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="Nweet" />
+    </form>
+  );
+};
 export default Home;
