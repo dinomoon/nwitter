089a3998a1b8940f4ea7ce40fc492fbefb8902e5
dinomoon <ansrud45@gmail.com>
2021-02-09 17:08:34 +0900

    2.6 Sign Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { authService } from '../fbase';
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  let history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Sign Out</button>
+    </>
+  );
+};
 export default Profile;
