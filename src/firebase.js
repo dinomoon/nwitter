c001b6da0a027620aff8899b20897a39787fc0a0
dinomoon <ansrud45@gmail.com>
2021-02-09 14:16:39 +0900

    1.0 initialize

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDljcMwOXaMWBLL2FrOjLgsDitrnaJ2PNc',
+  authDomain: 'nwitter-d7984.firebaseapp.com',
+  projectId: 'nwitter-d7984',
+  storageBucket: 'nwitter-d7984.appspot.com',
+  messagingSenderId: '260885004592',
+  appId: '1:260885004592:web:d3abfa112756eb1ec788f5',
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
