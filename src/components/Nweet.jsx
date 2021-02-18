2a62eaf4c2a1449fa5f2cec2ee5af83d0a663965
MoonK <ansrud45@gmail.com>
2021-02-19 00:42:11 +0900

    3.5 Delete and Update part Two

--- src/components/Nweet.jsx
+++ src/components/Nweet.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from '../fbase';
 
 const Nweet = ({ nweetObj, isOwner }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newText, setNewText] = useState(nweetObj.text);
+  const PATH = `nweets/${nweetObj.id}`;
+
+  const onDelete = () => {
+    const ok = window.confirm('are you sure delete this nweet?');
+    if (ok) {
+      dbService.doc(PATH).delete();
+    }
+  };
+
+  const onToggleEdit = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  const onChange = (e) => {
+    setNewText(e.target.value);
+  };
+
+  const onUpdate = async (e) => {
+    e.preventDefault();
+    await dbService.doc(PATH).update({ text: newText });
+    onToggleEdit();
+  };
+
   return (
     <>
-      <li>{nweetObj.text}</li>
-      {isOwner && (
+      {isEditing ? (
         <>
-          <button>Edit</button>
-          <button>Delete</button>
+          <form onSubmit={onUpdate}>
+            <input type="text" value={newText} onChange={onChange} />
+            <button type="submit">Update</button>
+          </form>
+          <button type="button" onClick={onToggleEdit}>
+            Cancel
+          </button>
+        </>
+      ) : (
+        <>
+          <li>{nweetObj.text}</li>
+          {isOwner && (
+            <>
+              <button onClick={onToggleEdit}>Edit</button>
+              <button onClick={onDelete}>Delete</button>
+            </>
+          )}
         </>
       )}
     </>
