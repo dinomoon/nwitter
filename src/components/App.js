6bc9d39af3325bcb55a252de90f7dc77e30102e4
MoonK <ansrud45@gmail.com>
2021-02-19 13:44:32 +0900

    3.6 Recap

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,13 @@ import { authService } from '../fbase';
 
 const App = () => {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     // 로그인을 했어도 처음에 들어올 때는 firebase가 로드되지 않아서 로그인했다고 확인을 못해서 useEffect와 onAuthStateChanged를 사용함.
     // 그리고 로그아웃했을 때, 바로 알아차릴 수 있게 onAuthStateChanged 사용함
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
@@ -22,7 +18,7 @@ const App = () => {
   return (
     <div>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
         'Initializing...'
       )}
